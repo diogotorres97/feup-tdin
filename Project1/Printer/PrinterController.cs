c0f6249cca156cff2231dc4a2b133fe96f3cc38d
darksystem <diogo.rey@hotmail.com>
2019-03-19 23:46:37 +0000

    Remove unused inheritance class

--- Project1/Printer/PrinterController.cs
+++ Project1/Printer/PrinterController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Remoting;
 
-public class PrinterController: MarshalByRefObject
+public class PrinterController
 {
     private IRestaurantSingleton _restaurantServer;
 
