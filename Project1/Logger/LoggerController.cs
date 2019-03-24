81c5b2f5cebd4db0e29717d89bfeca6a60c82a32
darksystem <diogo.rey@hotmail.com>
2019-03-24 09:52:01 +0000

    Added logger

---
+++ Project1/Logger/LoggerController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting;
+
+public class LoggerController
+{
+    private IRestaurantSingleton _restaurantServer;
+
+    public List<Order> Orders { get; }
+    public List<Product> Products { get; }
+    public List<Table> Tables { get; }
+
+    public LoggerController()
+    {
+        RemotingConfiguration.Configure("Logger.exe.config", false);
+        _restaurantServer = (IRestaurantSingleton) RemoteNew.New(typeof(IRestaurantSingleton));
+        Orders = _restaurantServer.GetListOfOrders();
+        Products = _restaurantServer.GetListOfProducts();
+        Tables = _restaurantServer.GetListOfTables();
+    }
+
+    public void AddOrderAlterEvent(AlterOrderDelegate alterOrderEvent)
+    {
+        _restaurantServer.AlterOrderEvent += alterOrderEvent;
+    }
+
+    public void RemoveOrderAlterEvent(AlterOrderDelegate alterOrderEvent)
+    {
+        _restaurantServer.AlterOrderEvent -= alterOrderEvent;
+    }
+
+    public void AddTableAlterEvent(AlterTableDelegate alterTableEvent)
+    {
+        _restaurantServer.AlterTableEvent += alterTableEvent;
+    }
+
+    public void RemoveTableAlterEvent(AlterTableDelegate alterTableEvent)
+    {
+        _restaurantServer.AlterTableEvent -= alterTableEvent;
+    }
+}
+
+/* Mechanism for instantiating a remote object through its interface, using the config file */
+
+static class RemoteNew
+{
+    private static Hashtable _types;
+
+    private static void InitTypeTable()
+    {
+        _types = new Hashtable();
+        foreach (WellKnownClientTypeEntry entry in RemotingConfiguration.GetRegisteredWellKnownClientTypes())
+            _types.Add(entry.ObjectType, entry);
+    }
+
+    public static object New(Type type)
+    {
+        if (_types == null)
+            InitTypeTable();
+        WellKnownClientTypeEntry entry = (WellKnownClientTypeEntry) _types[type];
+        if (entry == null)
+            throw new RemotingException("Type not found!");
+        return RemotingServices.Connect(type, entry.ObjectUrl);
+    }
+}
---
+++ Project1/Logger/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    private static LoggerController _loggerController;
+    private static AlterOrderEventRepeater _evOrderRepeater;
+    private static AlterTableEventRepeater _evTableRepeater;
+
+    private static void Main()
+    {
+        _loggerController = new LoggerController();
+        _evOrderRepeater = new AlterOrderEventRepeater();
+        _evOrderRepeater.AlterOrderEvent += DoOrderAlterations;
+        _loggerController.AddOrderAlterEvent(_evOrderRepeater.Repeater);
+
+        _evTableRepeater = new AlterTableEventRepeater();
+        _evTableRepeater.AlterTableEvent += DoTableAlterations;
+        _loggerController.AddTableAlterEvent(_evTableRepeater.Repeater);
+
+        Console.WriteLine("[Logger]");
+        Console.WriteLine("Press Enter to terminate.");
+        Console.ReadLine();
+
+        _evOrderRepeater.AlterOrderEvent -= DoOrderAlterations;
+        _loggerController.RemoveOrderAlterEvent(_evOrderRepeater.Repeater);
+
+        _evTableRepeater.AlterTableEvent -= DoTableAlterations;
+        _loggerController.RemoveTableAlterEvent(_evTableRepeater.Repeater);
+    }
+
+    /* Event handler for the remote AlterEvent subscription and other auxiliary methods */
+
+    private static void DoOrderAlterations(Operation op, Order order)
+    {
+        Console.WriteLine("[Logger]:  \"{0}\" -  \"{1}\"", op, order);
+    }
+
+    private static void DoTableAlterations(Operation op, Table table)
+    {
+        Console.WriteLine("[Logger]:  \"{0}\" -  \"{1}\"", op, table);
+    }
+}
