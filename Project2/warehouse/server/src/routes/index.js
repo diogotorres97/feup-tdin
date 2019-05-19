75fb9b58d377c31b0de1b98a63641a724f527705
darksystem <diogo.rey@hotmail.com>
2019-05-19 20:10:03 +0100

    Minor fix

--- Project2/warehouse/server/src/routes/index.js
+++ Project2/warehouse/server/src/routes/index.js
@@ -9,7 +9,7 @@ const auth = require('./auth');
 const books = require('./books');
 const requests = require('./requests');
 
-router.use('/', auth);
+router.use('/api/', auth);
 router.use('/api/', passport.authenticate('jwt', { session: false }));
 router.use('/api/', books);
 router.use('/api/', requests);
